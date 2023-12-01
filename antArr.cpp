44d5c0fce29c0ab94de856c600e43f05f1546db4
Vansh Vasishtha <74496392+ThatRandomGuy09@users.noreply.github.com>
2023-12-01 21:52:43 +0530

    Update antArr.cpp

--- antArr.cpp
+++ antArr.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
           int time = 0;      
-        for (int pos : left) {
+        for (int pos: left) {
             time = max(time, pos);
         }       
         for (int pos : right) {            
